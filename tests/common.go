cc3c52731d104b4cabf1ea4a1979d394aa0f50ba
rahulpinto19 <rahulyadav252424@gmail.com>
2026-01-09 06:53:10 +0000

    test

--- tests/common.go
+++ tests/common.go
@@ -969,7 +969,7 @@ func CleanupPostgresTables(t *testing.T, ctx context.Context, pool *pgxpool.Pool
 	if _, err := pool.Exec(ctx, dropQuery); err != nil {
 		t.Fatalf("Failed to drop all tables in 'public' schema: %v", err)
 	}
-	else { {
+	else {
 		t.Logf("Dropped tables in 'public' schema: %s", strings.Join(tablesToDrop, ", "))
 	}
 
